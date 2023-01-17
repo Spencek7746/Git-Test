0288451dc84d0e52e985dd6de94be06da5e3a27c
Spencer Karpati <spebkarp@ut.utm.edu>
2023-01-17 15:09:27 -0600

    Added comments and deleted redundant lines.

--- CW2 Getting Into Git/Program.cs
+++ CW2 Getting Into Git/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Name: Spencer Karpati
+//Instructor: Dr. Kathleen Ericson
+//Class: CSCI 352
+//Description: This program creates a random number guessing game.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,40 +15,37 @@ namespace CW2_Getting_Into_Git
     {
         static void Main(string[] args)
         {
-                Random randNum = new Random();
-                Console.WriteLine("I'm thinking of a number between 0 and 100...which number am I thinking of?");
+            Random randNum = new Random(); //Random number object is created.
+            Console.WriteLine("I'm thinking of a number between 0 and 100...which number am I thinking of?");
 
-                int max = 100;
-                int min = 0;
-                int num = randNum.Next(min, max);
-                int userTries = 0;
-                bool correct = false;
+            int max = 100;
+            int min = 0;
+            int num = randNum.Next(min, max); //Integer variable num is set to Random object
+            int userTries = 0;
+            bool correct = false; //To check if the user is correct. This will help with looping.
 
-                while (!correct)
-                {
-                String userNum = Console.ReadLine();
+            while (!correct) //While loop to grab constant user input as long as the user isn't correct.
+            {
+                String userNum = Console.ReadLine(); //Grab user input
 
                 if (Int32.Parse(userNum) > num)
-                    {
-                        Console.WriteLine("Number is higher than answer. Try again");
-                        userTries++;
-                    }
-
-                    else if (Int32.Parse(userNum) < num)
-                    {
-                        Console.WriteLine("Number is lower than answer. Try again");
-                        userTries++;
-                    }
-
-                    else if(Int32.Parse(userNum) == num)
-                    {
-                        Console.WriteLine("You've guessed correctly. Well done.");
-                        Console.WriteLine("Attempts: " + userTries);
-                    }
+                {
+                    Console.WriteLine("Number is higher than answer. Try again");
+                    userTries++;
                 }
 
-            Console.WriteLine("Press any key to continue");
-            Console.ReadKey();
+                else if (Int32.Parse(userNum) < num)
+                {
+                    Console.WriteLine("Number is lower than answer. Try again");
+                    userTries++;
+                }
+
+                else if (Int32.Parse(userNum) == num)
+                {
+                    Console.WriteLine("You've guessed correctly. Well done.");
+                    Console.WriteLine("Attempts: " + userTries);
+                }
             }
         }
     }
+}
